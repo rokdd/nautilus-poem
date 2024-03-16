19a06f1a77101145789a8c78d4fb4f4b2d710762
rokdd <rokdd@gmx.ch>
2024-03-16 10:19:51 +0100

    Better updater

--- install.sh
+++ install.sh
@@ -23,7 +23,10 @@ style_print "Start install.." 'gray'
 sudo $curl -H 'Cache-Control: no-cache' --silent https://raw.githubusercontent.com/rokdd/nautilus-poem/master/nautilus-poem.py > /usr/share/nautilus-python/extensions/nautilus-poem.py
 style_print "Installed nautilus-poem" 'green'
 mkdir -p "~/.config/nautilus-poem/"
+# only update file when it is not yet exists
+if [ ! -f "~/.config/nautilus-poem/items.yml" ]; then
 sudo $curl -H 'Cache-Control: no-cache' --silent https://raw.githubusercontent.com/rokdd/nautilus-poem/master/items.yml > ~/.config/nautilus-poem/items.yml
 style_print "Created config in ~/.config/nautilus-poem/"
+fi
 style_print "If cmd is not working, reload the nautilus with 'nautilus -q'. For help or getting started visit github.com/rokdd/nautilus-poem"
 exec $SHELL
